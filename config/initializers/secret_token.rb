bd200350d172afd4c1505bb4de644bbd93f4582b
domcerminara <dompkt2013@gmail.com>
2012-02-08 16:23:10 -0500

    Primary scaffold 0:35

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpineRailsTree::Application.config.secret_token = 'ee77f852c70b12475bc1aeff3db06e9350167fc789fabb33b16075339a510d02e91ecb791922e4dbf254122ca03f62ce66d63698ad6f02d887a1f8c44de9674c'
